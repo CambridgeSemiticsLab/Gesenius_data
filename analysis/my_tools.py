724f52f60ab0edb5099818db8916ed48354e40ad
Cody Kingham <codyakingham@gmail.com>
2020-10-28 17:00:37 +0000

    Add new tools

--- analysis/my_tools.py
+++ analysis/my_tools.py
@@ -10,6 +10,16 @@ repo_dir = Path.home().joinpath('github/CambridgeSemiticsLab/Gesenius_data')
 private_dir = repo_dir.joinpath('data/_private_')
 plots_dir = repo_dir.joinpath('analysis/plots/qatal')
 
+qatal_datapath = repo_dir.joinpath('data/_private_/verb_data/qatal_dataset.csv')
+qatal_df = pd.read_csv(qatal_datapath, index_col='bhsa_node')
+
+PUB_DIR = repo_dir.joinpath('data/_public_')
+allverb_datapath = PUB_DIR.joinpath('verb_data/allverb_bhsa.csv')
+av_df = pd.read_csv(allverb_datapath, index_col='bhsa_node')
+
+av_col_path = PUB_DIR.joinpath('verb_data/xverb_lexcollocations.csv')
+av_col_df = pd.read_csv(av_col_path, index_col='verb_form')
+
 def get_props(counts_df, sum_i=1, div_i=0):
     return counts_df.div(counts_df.sum(sum_i), div_i) 
 
@@ -62,12 +72,36 @@ class PivotProp:
         # calculate prop table
         self.pr = prop_table(self.ct, sumi=sum_i, divi=div_i)
 
-qatal_datapath = repo_dir.joinpath('data/_private_/verb_data/qatal_dataset.csv')
-qatal_df = pd.read_csv(qatal_datapath, index_col='bhsa_node')
+def pretty_hebrew(val):
+    """Render Hebrew in a dataframe."""
+    return 'font-size:20px; font-family: Times New Roman; text-align: right; max-width: 500px'
 
-PUB_DIR = repo_dir.joinpath('data/_public_')
-allverb_datapath = PUB_DIR.joinpath('verb_data/allverb_bhsa.csv')
-av_df = pd.read_csv(allverb_datapath, index_col='bhsa_node')
+def get_spread(array, n): 
+    """Retrieve an even spread of indices an array/Series.
+    
+    https://stackoverflow.com/a/50685454/8351428
+    
+    Args:
+        array: either numpy array or Pandas Series 
+            (with multiple indices allowed)
+        n: number of indices to return
+    Returns:
+        indexed array or series
+    """
+    end = len(array) - 1 
+    spread = np.ceil(np.linspace(0, end, n)).astype(int)
+    indices = np.unique(spread)
+    try:
+        return array[indices]
+    except KeyError:
+        return array.iloc[indices]
 
-av_col_path = PUB_DIR.joinpath('verb_data/xverb_lexcollocations.csv')
-av_col_df = pd.read_csv(av_col_path, index_col='verb_form')
+def show_text(df, col_default=['ref', 'sentence', 'text_full'], cols=[], spread=0):
+    """Display text from pandas dataframe in a readable way."""
+    original_shape = df.shape
+    df = df[col_default + cols]
+    if spread > 0:
+        spread_i = get_spread(df.index, spread)
+        df = df.loc[spread_i]
+    print(f'showing {df.shape[0]} of {original_shape[0]}')
+    return df.style.applymap(pretty_hebrew, subset=['text_full', 'sentence'])
