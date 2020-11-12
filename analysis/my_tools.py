840d0fbf1078fede438d4438aa26c846823652df
Cody Kingham <codyakingham@gmail.com>
2020-11-12 18:35:22 +0000

    New data, analysis, classes

--- analysis/my_tools.py
+++ analysis/my_tools.py
@@ -110,6 +110,24 @@ def get_spread(array, n):
     except KeyError:
         return array.iloc[indices]
 
+class TextShower:
+    """Show stylized text examples from a Dataframe of data"""
+    
+    def __init__(self, default=['ref', 'sentence', 'text_full'], 
+                 stylize=['sentence', 'text_full']):
+        self.default = default
+        self.stylize = stylize
+
+    def show(self, df, extra=[], spread=0):
+        """Display text from pandas dataframe in a readable way."""
+        original_shape = df.shape
+        df = df[self.default + extra]
+        if spread > 0:
+            spread_i = get_spread(df.index, spread)
+            df = df.loc[spread_i]
+        print(f'showing {df.shape[0]} of {original_shape[0]}')
+        return df.style.applymap(pretty_hebrew, subset=self.stylize)
+
 def show_text(df, col_default=['ref', 'sentence', 'text_full', 'lxx', 'lxx_tm', 'esv', 'esv_TAM'],
               cols=[], spread=0):
     """Display text from pandas dataframe in a readable way."""
